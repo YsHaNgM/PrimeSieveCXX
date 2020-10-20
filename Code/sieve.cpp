c771ae0974572df08432072c93ce325b9351e76a
YsHaNgM <ysh788@gmail.com>
2020-10-20 23:41:03 +0100

    init

--- Code/sieve.cpp
+++ Code/sieve.cpp
@@ -9,12 +9,14 @@
 
 // See README.md for instructions and information.
 
-using ptr = std::shared_ptr<double[]>; // gcc-7 is needed, for shared_ptr [] operand c++17 P0414R2
+using ptr = std::shared_ptr<int[]>; // gcc-7 is needed, for shared_ptr [] operand c++17 P0414R2
 
 int main(int argc, char *argv[])
 {
     auto n = 1000;
-    auto numbers = std::make_shared<int[]>(n);
+    ptr numbers(new int[n]);
     std::fill(numbers.get(), numbers.get() + n, 1);
+    auto print = [](const int &n) { std::cout << " " << n; };
+    std::for_each(numbers.get(), numbers.get() + n, print);
     return 0;
 }
